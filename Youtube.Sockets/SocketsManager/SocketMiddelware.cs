42506e5eda1bd23e116cb36ae9ff0c29a14c2d0c
Davorin <51224653+davorinjurkovic007@users.noreply.github.com>
2020-03-18 20:20:09 +0100

    First version commit

---
+++ Youtube.Sockets/SocketsManager/SocketMiddelware.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Youtube.Sockets.SocketsManager
+{
+    public class SocketMiddelware
+    {
+        private readonly RequestDelegate _next;
+        private SocketHandler Handler { get; set; }
+
+        public SocketMiddelware(RequestDelegate next, SocketHandler handler)
+        {
+            _next = next;
+            Handler = handler;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(!context.WebSockets.IsWebSocketRequest)
+            {
+                return;
+            }
+
+            var socket = await context.WebSockets.AcceptWebSocketAsync();
+            await Handler.OnConnected(socket);
+
+            await Receive(socket, async (result, buffer) =>
+            {
+                if(result.MessageType == WebSocketMessageType.Text)
+                {
+                    await Handler.Receive(socket, result, buffer);
+                    return; // ? toga nema u orginal video. Maknuti return ako ne stavi do kraja videa 
+                }
+                else if(result.MessageType == WebSocketMessageType.Binary)
+                {
+                    return;
+                }
+                else if(result.MessageType == WebSocketMessageType.Close)
+                {
+                    await Handler.OnDisconnected(socket);
+                    return;
+                }
+            });
+            
+        }
+
+        private async Task Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while(socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                                                        cancellationToken: CancellationToken.None);
+
+                handleMessage(result, buffer);
+            }
+        }
+    }
+}
